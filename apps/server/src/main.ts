14afa60550e3a1f3530d6aed11b03a4d24a373ac
Buğra Yüksel <bgryks18@gmail.com>
2022-10-10 19:53:41 +0300

    .env and cors

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -11,7 +11,7 @@ import * as cookieParser from 'cookie-parser'
 import * as express from 'express'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({ origin: 'http://localhost:3000' })
+  app.enableCors({ origin: 'http://localhost:3000', credentials: true })
   const globalPrefix = 'api'
   app.setGlobalPrefix(globalPrefix)
 
