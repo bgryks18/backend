c25b96bd3f8e05f746326989812b5b7073c62fca
Buğra Yüksel <bgryks18@gmail.com>
2022-10-14 20:19:07 +0300

    product form schema

--- apps/server/src/app/utils/product.validator.ts
+++ apps/server/src/app/utils/product.validator.ts
@@ -2,11 +2,11 @@ import * as yup from 'yup'
 
 export const ProductCreateSchema = yup.object().shape({
   name: yup.string().trim().required(),
-  categoryId: yup.number(),
+  categoryId: yup.number().nullable(),
   description: yup.string().required(),
   priority: yup.number().required(),
-  sliderId: yup.number(),
-  posterId: yup.number(),
+  sliderId: yup.number().nullable(),
+  posterId: yup.number().nullable(),
   seoTitle: yup.string().trim(),
   seoContent: yup.string().trim(),
 })
